4e07392a31e79e325fd0ba9bc409402982d027df
Arun Sapkota <arun.sapkota@accenturefederal.com>
2019-12-31 16:56:34 -0500

    adding user input

--- main/scala/Example/Main.scala
+++ main/scala/Example/Main.scala
@@ -3,4 +3,7 @@ package Example
 object Main extends App{
   val ages = Seq(42,88,29,64)
   println(s"The oldest person is ${ages.max}")
+  println("doing some more commits from branch")
+  val fromCmd = scala.io.StdIn.readLine()
+  println(s"the user put in ${fromCmd}")
 }
