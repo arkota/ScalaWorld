fa8bcc22dfb86d83f2ed1184ad22311cb657e4ae
Arun Sapkota <arun.sapkota@accenturefederal.com>
2020-01-02 10:37:30 -0500

    adding cube class and function to main

---
+++ main/scala/Example/CubeIt.scala
@@ -0,0 +1,9 @@
+package Example
+
+object CubeIt extends App {
+
+  def cube(x:Int) ={
+    println(s"cube of x is ${x*x*x}")
+  }
+  cube(4)
+}
--- main/scala/Example/Main.scala
+++ main/scala/Example/Main.scala
@@ -6,4 +6,5 @@ object Main extends App{
   println("doing some more commits from branch")
   val fromCmd = scala.io.StdIn.readLine()
   println(s"the user put in ${fromCmd}")
+  CubeIt.cube(3)
 }
